a2d543c95a56af30a6b2cf620c98138d3dca0cbb
mmalaquias1 <mateus.malaquias1@gmail.com>
2017-06-10 08:12:22 -0300

    Start-dev removed, JSON formartter removed and env PORT add

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -3,8 +3,7 @@
 const bodyParser = require('body-parser');
 
 module.exports = app => {
-	app.set('port', 3000);
-	app.set('json spaces', 4);
+	app.set('port', process.env.PORT || 3000);
 	app.use(bodyParser.json());
 	app.use((req, res, next) => {
 		delete req.body.id;
